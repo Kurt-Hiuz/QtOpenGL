289627be999ba0b870a47e1072e1a095b6494c1f
Peven Dmitriy <dima.peven@yandex.ru>
2025-04-04 01:38:19 +0300

    Забыл закинуть

---
+++ 1_lab/appMenu/appmenu.h
@@ -0,0 +1,26 @@
+#ifndef APPMENU_H
+#define APPMENU_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+class AppMenu : public QWidget
+{
+    Q_OBJECT
+public:
+    AppMenu(QWidget *parent = nullptr);
+    void createInterface();
+    QPushButton* getClearPainterBtn() const { return clearPainterBtn; }
+    QPushButton* getAddNewFigureBtn() const { return addNewFigureBtn; }
+
+private:
+    QPushButton *addNewFigureBtn;
+    QPushButton *editRedoBtn;
+    QPushButton *editUndoBtn;
+    QPushButton *clearPainterBtn;
+
+    QVBoxLayout *verticalLayout;
+};
+
+#endif // APPMENU_H
